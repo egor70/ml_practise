e7cec9fb942e986cbd8fe806f3b799f34f375b8a
egor70 <124879522+egor70@users.noreply.github.com>
2026-03-30 23:16:25 +0300

    Update mnist_easy.py

--- mnist_easy.py
+++ mnist_easy.py
@@ -1,69 +1,61 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from tensorflow import keras
-from tensorflow.keras import layers
+
+# Setting random seeds to get reproducible results
+np.random.seed(0)
+import tensorflow as tf
+tf.random.set_seed(1)
+
+
+fig = plt.figure(figsize=(20,20))
+for i in range(5):
+    ax = fig.add_subplot(1, 5, i+1, xticks=[], yticks=[])
+    ax.imshow(x_train[i], cmap='Greys')
+    ax.set_title('Label:' + str(y_train[i]))
+
+
+
+x_train_reshaped = x_train.reshape(-1, 28*28)
+x_test_reshaped = x_test.reshape(-1, 28*28)
 from tensorflow.keras.utils import to_categorical
+y_train_cat = to_categorical(y_train, 10)
+y_test_cat = to_categorical(y_test, 10)
+# Imports
+#import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+#from tensorflow.keras.layers import Dense, Dropout, Activation
+#from tensorflow.keras.optimizers import SGD
 
-# 1. ЗАГРУЗКА ДАННЫХ
-print("Загрузка данных MNIST...")
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+# Building the model
+model = Sequential()
+model.add(Dense(128, activation='relu', input_shape=(28*28,)))
+model.add(Dropout(.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(.2))
+model.add(Dense(10, activation='softmax'))
 
-# 2. ПРЕДОБРАБОТКА
-# Нормализация (0-255 -> 0-1)
-x_train = x_train.astype('float32') / 255.0
-x_test = x_test.astype('float32') / 255.0
-
-# Преобразование из 28x28 в 784 пикселя
-x_train = x_train.reshape(x_train.shape[0], 784)
-x_test = x_test.reshape(x_test.shape[0], 784)
-
-# Преобразование меток в one-hot encoding
-y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
-
-print(f"Обучающая выборка: {x_train.shape}")
-print(f"Тестовая выборка: {x_test.shape}")
-
-# 3. СОЗДАНИЕ МОДЕЛИ
-model = keras.Sequential([
-    layers.Input(shape=(784,)),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(10, activation='softmax')
-])
-
-# 4. КОМПИЛЯЦИЯ
-model.compile(
-    optimizer='adam',
-    loss='categorical_crossentropy',
-    metrics=['accuracy']
-)
-
-# Вывод архитектуры
-print("\nАрхитектура модели:")
+# Compiling the model
+model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
-# 5. ОБУЧЕНИЕ
-print("\nНачало обучения...")
-history = model.fit(
-    x_train, y_train,
-    batch_size=32,
-    epochs=5,
-    validation_split=0.2,
-    verbose=1
-)
 
-# 6. ОЦЕНКА
-test_loss, test_accuracy = model.evaluate(x_test, y_test, verbose=0)
-print(f"\nТочность на тестовых данных: {test_accuracy:.4f}")
-print(f"Потери на тестовых данных: {test_loss:.4f}")
 
-# 7. ПРИМЕР ПРЕДСКАЗАНИЯ
-# Берем первое изображение из тестовой выборки
-sample = x_test[0:1]  # форма (1, 784)
-prediction = model.predict(sample, verbose=0)
-predicted_digit = np.argmax(prediction)
-true_digit = np.argmax(y_test[0])
+model.fit(x_train_reshaped, y_train_cat, epochs=10, batch_size=10)
+predictions_vector = model.predict(x_test_reshaped)
+predictions = [np.argmax(pred) for pred in predictions_vector]
+plt.imshow(x_test[4], cmap='Greys')
+plt.xticks([])
+plt.yticks([])
+print("The label is", y_test[4])
+print("The prediction is", predictions[4])
 
-print(f"\nПример предсказания:")
-print(f"Предсказанная цифра: {predicted_digit}")
-print(f"Истинная цифра: {true_digit}")
+
+
+plt.imshow(x_test[18], cmap='Greys')
+plt.xticks([])
+plt.yticks([])
+print("The label is", y_test[18])
+print("The prediction is", predictions[18])
